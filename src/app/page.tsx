d8f9f6736d3930dddd25e46b1769d08c91836869
sajib-hosen <sajib.201h@gmail.com>
2023-05-10 16:50:32 +0600

    worked on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,17 @@ import Image from 'next/image';
 import NavigationBar from '@/components/shared/navigationBar/NavigationBar';
 import logo from './../assets/box.png'
 import HomeSection from '@/components/home/HomeSection';
-import bird from './../assets/logo.png'
+import bird from './../assets/logo.png';
+import building from './../assets/building.png';
+import fusion from './../assets/fusion.png';
+import ecomm from './../assets/ecommerce.png';
+import erp from './../assets/erp.png';
 
 
 export default function Home() {
+
+    
+
   return (
     <main className="page-root  ">
       <NavigationBar/>
@@ -14,18 +21,40 @@ export default function Home() {
         <HomeSection
             title='Complex made simple'
             subHead='Grow Your Business with our PRO-efficient Products'
-            textBody='Using our resourceful and intelligent management tools you can automate your complex workflow and save time to invest in taking your business to a whole new level.'
+            textBody={['Using our resourceful and intelligent management tools you can automate your complex workflow and save time to invest in taking your business to a whole new level.']}
             imgUrl={bird}
             imgSide='r'
         />
+
+        <div className='my-10' ></div>
         <HomeSection
-            title='Complex made simple'
-            subHead='Grow Your Business with our PRO-efficient Products'
-            textBody='Using our resourceful and intelligent management tools you can automate your complex workflow and save time to invest in taking your business to a whole new level.'
-            imgUrl={bird}
+            title='Duck Fusion'
+            subHead='Our very own inception for order automation'
+            textBody={['State of the art solution for automation of orders via API, client & supplier management, Billing, Shopping cart, Blog, Live Chat and Support tickets all under one dashboard.']}
+            imgUrl={fusion}
             imgSide='l'
         />
 
+        <div className='my-10' ></div>
+        <HomeSection
+            title='Duck ERP'
+            subHead='An ideal solution for Retail store, Restaurant and Service centers'
+            textBody={['Ecosystem ERP designed specially by recognizing the demanding needs of the upcoming, small and midsize enterprises.', 'Our cloud-based ERP solution helps you manage all your business needs from accounting, CRM, Point Of Sale to Supply chain management, advanced inventory management , support/service Tickets and Intuitive & precise Reports']}
+            imgUrl={erp}
+            imgSide='r'
+        />
+        
+        <div className='my-10' ></div>
+        <HomeSection
+            title='Duck Ecommerce'
+            subHead='Secure & Powerful'
+            textBody={["Hosted solution built on Dhru's Robust Framework.", 'Packed with amazing security measures, optimized performance and flexibility to customize all your out of the box needs.']}
+            imgUrl={ecomm}
+            imgSide='l'
+        />
+
+
+        {/* https://www.dhru.com/ */}
 
       </div>
     </main>
--- src/components/home/HomeSection.tsx
+++ src/components/home/HomeSection.tsx
@@ -4,22 +4,23 @@ import React from 'react';
 interface HomeSectionTy {
     title: string,
     subHead:string,
-    textBody: string,
+    textBody: string[],
     imgUrl: StaticImageData,
     imgSide?: 'r' | 'l',
 }
 
 const HomeSection = ({ title, subHead, textBody, imgUrl, imgSide }:HomeSectionTy) => {
     return (
-        <div className='' >
+        <div className='py-4 px-2' >
             <div className={`flex items-center ${imgSide ? imgSide==='l' ? 'flex-row-reverse' :'' : ''}`} >
                 <div className='w-1/2' >
                     <h1 className='text-3xl font-bold mb-3 ' >{title}</h1>
                     <h4 className='text-xl font-semibold mb-3' >{subHead}</h4>
-                    <p className=' text-justify' >{textBody}</p>
+                    { textBody.length ? textBody.map((each, i)=>(<p key={i} className=' text-justify' >{each}</p>)) : null}
                 </div>
-                <div className='w-1/2' >
+                <div className='w-1/2 flex justify-around items-center' >
                     <Image
+                        width={450}
                         src={imgUrl}
                         alt="body section picture"
                     />
