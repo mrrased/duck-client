6b27ea6433aa0558668f895dcb67ef20048d09b2
sajib-hosen <sajib.201h@gmail.com>
2023-05-27 16:25:39 +0600

    worked on nav dropdown

--- src/app/(publicPage)/login/page.tsx
+++ src/app/(publicPage)/login/page.tsx
@@ -29,7 +29,8 @@ const Login = () => {
     return (
         <div className={` page-root flex justify-center items-center `} >
 
-            <div className=' bg-blue-800 shadow-2xl shadow-indigo-700 rounded-md bg-opacity-50 bg-blur-lg ' >
+            <div className=' bg-blue-400 shadow-2xl shadow-indigo-700 rounded-md bg-opacity-50 bg-blur-lg ' >
+                
                 <div className=' flex justify-around items-center flex-col mt-4 ' >
                     <div className='flex justify-center items-center ' >
                         <Image width={70} src={logo} alt='logo' />
@@ -37,15 +38,17 @@ const Login = () => {
                     </div>
                     <p className='text-gray-700' >Let's build your business.</p>
                 </div>
-                <form onSubmit={(e)=>handleLogin} className=' flex flex-col p-5 ' >
+
+                <div onSubmit={(e)=>handleLogin} className=' flex flex-col p-5 ' >
                     <input onChange={(e)=>{getLoginInfo(e)}} className='p-2 my-4 rounded-md w-80 ' type="text" name="email" id="email" placeholder='E-mail' />
-                    <input onChange={(e)=>{getLoginInfo(e)}}  className='p-2 mt-4 rounded-md w-80  ' type="password" name="password" id="password" placeholder='Password' />
+                    <input onChange={(e)=>{getLoginInfo(e)}}  className='p-2 mt-4 rounded-md w-80 ' type="password" name="password" id="password" placeholder='Password' />
                     <div className='flex justify-between px-2 text-gray-800 mt-2 ' >
                         <p>Forgot password?</p>
                         <p>Register</p>
                     </div>
                     <input className='mt-10 mb-6 hover:shadow-xl cursor-pointer text-white p-2 my-4 rounded-md w-80 bg-slate-500 ' type="submit" value="Login" />
-                </form>
+                </div>
+
             </div>
 
         </div>
--- src/components/shared/navigationBar/NavigationBar.tsx
+++ src/components/shared/navigationBar/NavigationBar.tsx
@@ -9,10 +9,10 @@ import logo from "./../../../assets/logo.png";
 
 const NavigationBar = () => {
     const currentPath = usePathname()
-    console.log( currentPath )
+    const [productClicked, setProductClice] = React.useState(true);
 
     return (
-        <div className=' sticky top-0 bg-blue-900 bg-opacity-30 backdrop-blur-md w-screen flex justify-around overflow-x-hidden ' >
+        <div className=' sticky top-0 bg-blue-900 bg-opacity-30 backdrop-blur-md w-screen flex justify-around ' >
             <div className='body-size flex justify-between items-center '>
                 <Link href={'/'} >
                     <div className='flex items-center ' >
@@ -20,15 +20,37 @@ const NavigationBar = () => {
                         <h1 className='text-2xl ml-2 font-bold' >Duck</h1>
                     </div>
                 </Link>
-                <nav className=' font-semibold' >
+                <nav className=' flex font-semibold' >
 
-                    {/* <Link href={'/'} >
-                        <span className='nav-link' >Home</span>
+                    <div className='relative ' >
+                        {/* <button >hi</button> */}
+                        <span
+                            tabIndex={1}
+                            onClick={()=>setProductClice(true) }
+                            className={` nav-link cursor-pointer `} 
+                        >Products</span>
+
+                        <div onClick={()=>setProductClice(false)} className={`${ productClicked ? "" : "hidden" } fixed inset-0 h-screen w-screen bg-black bg-opacity-0 `} ></div>
+
+                        <div
+                            tabIndex={1}
+                            className={`${ productClicked ? "" : "hidden" } absolute flex flex-col bg-opacity-80 backdrop-blur-md mt-2 py-3 w-60 rounded text-white z-auto bg-slate-500 `}
+                        >
+                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck Fusion</span>
+                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck Ecommerce</span>
+                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck Wallet</span>
+                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck ERP for Restaurant</span>
+                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck ERP for Retail</span>
+                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck ERP for Service center</span>
+                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck ERP for School</span>
+                        </div>
+
+                    </div>
+
+                    {/* <Link href={'/products'} >
+                        <span className={`${currentPath.startsWith('/products') ? "text-lime-300 underline " : ""} nav-link`} >Products</span>
                     </Link> */}
 
-                    <Link href={'/products'} >
-                        <span className={`${currentPath.startsWith('/products') ? "text-lime-300 underline " : ""} nav-link`} >Products</span>
-                    </Link>
                     <Link href={'/duck-fusion'} >
                         <span className={`${currentPath.startsWith('/duck-fusion') ? "text-lime-300 underline " : ""} nav-link`} >Duck Fusion</span>
                     </Link>
