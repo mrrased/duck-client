650e873c67076936aeaf8f69a8c472f5e2de999a
sajib-hosen <sajib.201h@gmail.com>
2023-05-28 12:16:19 +0600

    worked on title  and nav link

--- src/components/shared/navigationBar/NavigationBar.tsx
+++ src/components/shared/navigationBar/NavigationBar.tsx
@@ -3,13 +3,14 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import logo from "./../../../assets/logo.png";
 
 
 const NavigationBar = () => {
+    const router = useRouter();
     const currentPath = usePathname()
-    const [productClicked, setProductClice] = React.useState(true);
+    const [productClicked, setProductClice] = React.useState(false);
 
     return (
         <div className=' sticky top-0 bg-blue-900 bg-opacity-30 backdrop-blur-md w-screen flex justify-around ' >
@@ -27,7 +28,7 @@ const NavigationBar = () => {
                         <span
                             tabIndex={1}
                             onClick={()=>setProductClice(true) }
-                            className={` nav-link cursor-pointer `} 
+                            className={`${currentPath.startsWith('/products') ? "text-lime-300 underline " : ""} nav-link cursor-pointer `} 
                         >Products</span>
 
                         <div onClick={()=>setProductClice(false)} className={`${ productClicked ? "" : "hidden" } fixed inset-0 h-screen w-screen bg-black bg-opacity-0 `} ></div>
@@ -36,24 +37,38 @@ const NavigationBar = () => {
                             tabIndex={1}
                             className={`${ productClicked ? "" : "hidden" } absolute flex flex-col bg-opacity-80 backdrop-blur-md mt-2 py-3 w-60 rounded text-white z-auto bg-slate-500 `}
                         >
-                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck Fusion</span>
-                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck Ecommerce</span>
-                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck Wallet</span>
-                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck ERP for Restaurant</span>
-                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck ERP for Retail</span>
-                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck ERP for Service center</span>
-                           <span onClick={()=>setProductClice(false)} className='text-black py-2 px-2 hover:bg-gray-300 bg-opacity-50 font-semibold '>Duck ERP for School</span>
+                           <span onClick={()=>{
+                                setProductClice(false);
+                                router.push('/products/duck-fusion')
+                           }} className='nav-drop-down '>Duck Fusion</span>
+                           <span onClick={()=>{
+                                setProductClice(false);
+                                router.push('/products/duck-ecommerce')
+                           }} className='nav-drop-down '>Duck Ecommerce</span>
+                           <span onClick={()=>{
+                                setProductClice(false);
+                                router.push('/products/duck-wallet')
+                           }} className='nav-drop-down '>Duck Wallet</span>
+                           <span onClick={()=>{
+                                setProductClice(false);
+                                router.push('/products/duck-erp-restaurant')
+                           }} className='nav-drop-down '>Duck ERP for Restaurant</span>
+                           <span onClick={()=>{
+                                setProductClice(false);
+                                router.push('/products/duck-erp-retail')
+                           }} className='nav-drop-down '>Duck ERP for Retail</span>
+                           <span onClick={()=>{
+                                setProductClice(false);
+                                router.push('/products/duck-erp-service')
+                           }} className='nav-drop-down '>Duck ERP for Service Center</span>
+                           <span onClick={()=>{
+                                setProductClice(false);
+                                router.push('/products/duck-erp-school')
+                           }} className='nav-drop-down '>Duck ERP for School</span>
                         </div>
 
                     </div>
 
-                    {/* <Link href={'/products'} >
-                        <span className={`${currentPath.startsWith('/products') ? "text-lime-300 underline " : ""} nav-link`} >Products</span>
-                    </Link> */}
-
-                    <Link href={'/duck-fusion'} >
-                        <span className={`${currentPath.startsWith('/duck-fusion') ? "text-lime-300 underline " : ""} nav-link`} >Duck Fusion</span>
-                    </Link>
                     <Link href={'/services'} >
                         <span className={`${currentPath.startsWith('/services') ? "text-lime-300 underline " : ""} nav-link`} >Services</span>
                     </Link>
